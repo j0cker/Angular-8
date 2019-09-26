43a4d99cc31502480df8e983c0c37f38d4bc53a3
Manlio Emiliano Terán Ramos <manlioelnum1@hotmail.com>
2019-09-26 14:36:51 -0500

    post example

--- Angular1/src/app/DAO/paises.service.ts
+++ Angular1/src/app/DAO/paises.service.ts
@@ -16,6 +16,25 @@ export class PaisesService {
     getPaises():any {
 
       return this.http.get(this.url);
+
+      /*
+      this.httpClient.post("http://127.0.0.1:3000/customers",{
+      "name":  "Customer004",
+      "email":  "customer004@email.com",
+      "tel":  "0000252525"
+      })
+      .subscribe(
+      data  => {
+      console.log("POST Request is successful ", data);
+      },
+      error  => {
+
+      console.log("Error", error);
+
+      }
+
+      );
+      */
     }
 
 }
